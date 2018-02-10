50d53548eb99dee22be02e62efdb868ff25265a9
sylcz <22798872+sylcz@users.noreply.github.com>
2018-02-10 13:08:51 +0000

    Update FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -2,7 +2,7 @@ public class FizzBuzz
 {
 	public static void main(String[] args) 
 	{
-		for(int i = 0; i < 101; i++) 
+		for(int i = 1; i < 101; i++) 
 		{
 			System.out.println(i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i);
 		}
