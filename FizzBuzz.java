3ae10a1698ed31da5206fa1858483da810d35434
sylcz <sylwur@gmail.com>
2018-02-10 13:06:30 +0000

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,10 @@
+public class FizzBuzz 
+{
+	public static void main(String[] args) 
+	{
+		for(int i = 0; i < 101; i++) 
+		{
+			System.out.println(i % 15 == 0 ? "FizzBuzz" : i % 3 == 0 ? "Fizz" : i % 5 == 0 ? "Buzz" : i);
+		}
+	}
+}
